004b46c5fda3adb9fe7be0887423b02cd10bea7f
Sojeong Park <macpark09@gmail.com>
2021-08-28 19:51:48 +0100

    implement react-leaflet

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
+
+
 
 const Home = () => {
+
+
     return (
+        
         <div>
-            
-        </div>
+            <h1>What's happening in your area?</h1>
+            <h2>Find your local area </h2>
+             <input></input>
+             
+             <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+  <TileLayer
+    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[51.505, -0.09]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
+</div> 
+          
+        
     );
 };
-
+ 
 export default Home;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Nav = () => {
     return (
         <div>
-            
+           <h3>Statistics</h3> 
+           <h3>Crime Map</h3> 
+           <h3>Police Workforce</h3> 
+           
         </div>
     );
 };
