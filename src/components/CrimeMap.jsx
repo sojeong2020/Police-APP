c44a6bc485be42702d1b94d060946024e6d6db15
Sojeong Park <macpark09@gmail.com>
2021-08-29 11:56:38 +0100

    connection to postcode API

--- src/components/CrimeMap.jsx
+++ src/components/CrimeMap.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import {useState,useEffect} from 'react';
+import { getOutcomes } from '../utils/api';
+
 
 const CrimeMap = () => {
+    const [outcomes, setOutcomes] = useState([]);
+
+    useEffect(()=>{
+        getOutcomes().then((outcomesFromApi)=>{
+            setOutcomes(outcomesFromApi)
+        })
+    },[])
+
     return (
         <div>
             <h1>crime map</h1>
+            <h3>{outcomes.length} crimes were reported here in </h3>
         </div>
     );
 };
