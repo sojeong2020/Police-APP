fdd8466252f74399e361757cb2fd501de956a179
Sojeong Park <macpark09@gmail.com>
2021-09-02 13:09:50 +0100

    add container-fluid class

--- src/components/StopSearch.jsx
+++ src/components/StopSearch.jsx
@@ -7,16 +7,32 @@ import BarChart from '../charts/BarChart';
 const StopSearch = ({force,date}) => {
     
     return (
-        <div>
-            <h1>Stop and search</h1>
+        <div className="container-fluid">
+            
+            <div className="row">
+            <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 p-1">
+            <div className="card shadow mb-xl-2 mb-lg-2 mg-md-2 mb-sm-2 mb-2">
+
             <h2>Age range</h2>
             <RadarChart force={force} date={date}/>
+
+            </div>
+            </div>
+
+            <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 p-1">
+            <div className="card shadow mb-xl-2 mb-lg-2 mg-md-2 mb-sm-2 mb-2">
             <h2>Self defined ethnicity</h2>
             <PieChart force={force} date={date}/>
+            </div>
+            </div>
+
+            <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 p-1">
+            <div className="card shadow mb-xl-2 mb-lg-2 mg-md-2 mb-sm-2 mb-2">
             <h2>Outcome</h2>
             <BarChart force={force} date={date}/>
-            
-            
+            </div>
+            </div>
+            </div>
         </div>
     );
 };
